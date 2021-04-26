396b9d40e4d90d13d02f5286d323cf73b1d072dc
Sascha Depold <sdepold@ebay.com>
2021-04-26 15:24:01 +0200

    Remove api

--- api/index.js
+++
@@ -1,6 +0,0 @@
-const server = require("./src/server")();
-const port = process.env.PORT || 5000;
-
-server.listen(port, () => {
-  console.log(`Fibonacci API is now listening on port ${port}!`);
-});
