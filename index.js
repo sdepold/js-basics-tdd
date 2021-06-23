1c5fd11de96b2b010e6d4ae581dd777cf40f5610
Sascha Depold <sdepold@ebay.com>
2021-06-23 13:05:47 +0200

    Add tests/implementation of fib

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = function fib(n) {};
+module.exports = function fib(n) {
+  if (typeof n !== "number" || n % 1 !== 0 || n < 0) {
+    // use case: invalid input causes undefined
+    // return undefined;
+
+    // use case: invalid input causes exception
+    throw new Error("invalid input!");
+  }
+
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+};
--- test.js
+++ test.js
@@ -1,2 +1,34 @@
 const fib = require("./index");
 const { expect } = require("chai");
+
+describe("fib", () => {
+  it("should return 0 for index 0", () => {
+    expect(fib(0)).to.equal(0);
+  });
+
+  it("should return 1 for index 1", () => {
+    expect(fib(1)).to.equal(1);
+  });
+
+  it("should return 5 for index 5", () => {
+    expect(fib(5)).to.equal(5);
+  });
+
+  it("should return 21 for index 8", () => {
+    expect(fib(8)).to.equal(21);
+  });
+
+  it("calculates the golden ratio if two consecutive fib numbers are divided", () => {
+    expect(fib(11) / fib(10)).to.be.closeTo(1.618, 0.1);
+  });
+
+  // use case: invalid input causes undefined
+  xit("should return undefined for negative numbers", () => {
+    expect(fib(-1)).to.be.undefined;
+  });
+
+  // use case: invalid input causes exception
+  it("should return undefined for negative numbers", () => {
+    expect(() => fib(-1)).to.throw("invalid input!");
+  });
+});
